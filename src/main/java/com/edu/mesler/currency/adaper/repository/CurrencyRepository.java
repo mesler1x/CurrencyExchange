9e108b84a167769034286d97252bdd403a67d0c5
Roman Mesler <pay2mesler@gmail.com>
2024-05-06 08:31:34 +0500

    fix jdbc template

--- src/main/java/com/edu/mesler/currency/adaper/repository/CurrencyRepository.java
+++ src/main/java/com/edu/mesler/currency/adaper/repository/CurrencyRepository.java
@@ -4,17 +4,14 @@ import com.edu.mesler.currency.adaper.repository.mapper.CurrencyMapper;
 import com.edu.mesler.currency.adaper.web.dto.CurrencyRequest;
 import com.edu.mesler.currency.adaper.web.dto.CurrencyResponse;
 import com.edu.mesler.currency.adaper.web.exception.CurrencyNotFoundException;
-import com.edu.mesler.currency.adaper.web.exception.DBException;
 import com.edu.mesler.currency.domain.Currency;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
-import lombok.SneakyThrows;
 import lombok.experimental.FieldDefaults;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 
-import java.sql.*;
+import java.sql.Connection;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -40,43 +37,26 @@ public class CurrencyRepository {
         return response;
     }
 
-    @SneakyThrows
+
     public CurrencyResponse getOneByCode(String code) {
-        try {
-            PreparedStatement statement = connection.prepareStatement("SELECT * FROM Currencies WHERE code = ?");
-            statement.setString(1, code);
-            ResultSet resultSet = statement.executeQuery();
-            while (resultSet.next()) {
-                int id = resultSet.getInt("id");
-                String fullName = resultSet.getString("fullName");
-                String sign = resultSet.getString("sign");
-                return new CurrencyResponse(id, code, fullName, sign);
-            }
-        } catch (SQLException e) {
-            throw new DBException();
+        Currency queryResult = jdbcTemplate.query("SELECT * FROM Currencies WHERE code = ?",
+                new Object[]{code},
+                new CurrencyMapper()).stream().findFirst().orElse(null);
+
+        if(queryResult == null) {
+            throw new CurrencyNotFoundException();
         }
 
-        throw new CurrencyNotFoundException();
+        return new CurrencyResponse(queryResult.getId(),
+                queryResult.getFullName(),
+                queryResult.getCode(),
+                queryResult.getSign());
     }
 
-    @SneakyThrows
     public CurrencyResponse create(CurrencyRequest currencyRequest) {
-        CurrencyResponse currencyResponse;
-        try {
-            PreparedStatement preparedStatement =
-                    connection.prepareStatement("INSERT INTO Currencies(code, fullName, sign) VALUES (?,?,?)");
+        jdbcTemplate.update("INSERT INTO Currencies (code, fullName, sign) VALUES (?,?,?)",
+                currencyRequest.code(), currencyRequest.name(), currencyRequest.sign());
 
-            preparedStatement.setString(1, currencyRequest.code());
-            preparedStatement.setString(2, currencyRequest.name());
-            preparedStatement.setString(3, currencyRequest.sign());
-            preparedStatement.executeUpdate();
-
-            currencyResponse = getOneByCode(currencyRequest.code());
-
-
-        } catch (SQLException e) {
-            throw new DBException();
-        }
-        return currencyResponse;
+        return getOneByCode(currencyRequest.code());
     }
 }
