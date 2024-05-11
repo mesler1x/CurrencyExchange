f9c2c709484368c61f05facb40730e340cd8bd4b
Roman Mesler <pay2mesler@gmail.com>
2024-05-11 18:37:36 +0500

    add openapi docs

--- src/main/java/com/edu/mesler/currency/adaper/web/controller/CurrencyController.java
+++ src/main/java/com/edu/mesler/currency/adaper/web/controller/CurrencyController.java
@@ -3,6 +3,8 @@ package com.edu.mesler.currency.adaper.web.controller;
 import com.edu.mesler.currency.adaper.web.dto.request.CurrencyRequest;
 import com.edu.mesler.currency.adaper.web.dto.response.CurrencyResponse;
 import com.edu.mesler.currency.service.CurrencyService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
@@ -15,19 +17,32 @@ import java.util.List;
 @RestController
 @RequiredArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+@Tag(name = "Контроллер валют.", description = "Контроллер для управления валютой.")
 public class CurrencyController {
     CurrencyService currencyService;
 
+    @Operation(
+            summary = "Просмотр всех валют.",
+            description = "Ендпоинт отдаёт все валюты, котрые находятся в базе данных."
+    )
     @GetMapping("/currencies")
     public List<CurrencyResponse> getAll() {
         return currencyService.getAllCurrencies();
     }
 
+    @Operation(
+            summary = "Просмотр конкретной валюты валюты.",
+            description = "Просмотр валюты по её трёхсимвольному коду в формате - GET /currency/USD"
+    )
     @GetMapping("/currency/{code}")
     public CurrencyResponse getOne(@PathVariable String code) {
         return currencyService.getCurrencyByCode(code);
     }
 
+    @Operation(
+            summary = "Создание валюты.",
+            description = "Ендпоинт для создания новой валюты."
+    )
     @PostMapping("/currencies")
     public ResponseEntity<CurrencyResponse> create(@RequestBody CurrencyRequest currencyRequest) {
         return new ResponseEntity<>(currencyService.createCurrency(currencyRequest), HttpStatus.CREATED) ;
--- src/main/java/com/edu/mesler/currency/adaper/web/controller/ExchangeController.java
+++ src/main/java/com/edu/mesler/currency/adaper/web/controller/ExchangeController.java
@@ -5,6 +5,8 @@ import com.edu.mesler.currency.adaper.web.dto.request.ExchangeRateAddRequest;
 import com.edu.mesler.currency.adaper.web.dto.response.ExchangeConvertedResponse;
 import com.edu.mesler.currency.adaper.web.dto.response.ExchangeResponse;
 import com.edu.mesler.currency.service.ExchangeService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
@@ -17,29 +19,50 @@ import java.util.List;
 @RestController
 @FieldDefaults(makeFinal = true, level = AccessLevel.PRIVATE)
 @RequiredArgsConstructor
+@Tag(name = "Контроллер обмена валют.", description = "Контроллер, позволяющий управлять обменными курсами валют.")
 public class ExchangeController {
     ExchangeService exchangeService;
 
+    @Operation(
+            summary = "Просмотр всех обменных курсов.",
+            description = "Получение списка всех обменных курсов."
+    )
     @GetMapping("/exchangeRates")
     public List<ExchangeResponse> getAllExchanges() {
         return exchangeService.getAllExchanges();
     }
 
+    @Operation(
+            summary = "Получение конкретного обменного курса.",
+            description = "Получение конкретного обменного курса. Валютная пара задаётся идущими подряд кодами валют в адресе запроса. Пример - GET /exchangeRate/USDRUB"
+    )
     @GetMapping("/exchangeRate/{codes}")
     public ExchangeResponse getExchangeRate(@PathVariable String codes) {
         return exchangeService.getExchangeByCodes(codes);
     }
 
+    @Operation(
+            summary = "Создание нового обменного курса.",
+            description = "Добавление нового обменного курса в базу."
+    )
     @PostMapping("/exchangeRates")
     public ResponseEntity<ExchangeResponse> createExchangeRate(@RequestBody ExchangeRateAddRequest exchangeRateAddRequest) {
         return new ResponseEntity<>(exchangeService.addNewExchangeRate(exchangeRateAddRequest), HttpStatus.CREATED);
     }
 
+    @Operation(
+            summary = "Обновление существующего обменного курса",
+            description = "Обновление существующего в базе обменного курса. Валютная пара задаётся идущими подряд кодами валют в адресе запроса."
+    )
     @PatchMapping("/exchangeRate/{codes}")
     public ExchangeResponse editExistingExchangeRate(@PathVariable String codes, @RequestBody ExchangeEditRequest exchangeEditRequest) {
         return exchangeService.editExchangeRate(codes, exchangeEditRequest.rate());
     }
 
+    @Operation(
+            summary = "Расчёт перевода определённого количества средств из одной валюты в другую.",
+            description = "Пример запроса - GET /exchange?from=USD&to=AUD&amount=10"
+    )
     @GetMapping("/exchange")
     public ExchangeConvertedResponse getExchangeRateAnyAmount(@RequestParam String from, @RequestParam String to, @RequestParam String amount) {
         return exchangeService.getExchangeAnyAmount(from, to, amount);
